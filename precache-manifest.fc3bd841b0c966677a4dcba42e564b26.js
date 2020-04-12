783fa0a53304c19be8a49d3e27ce31673160ef5a
Lam Tran <lam.tran@commercetools.de>
2020-04-12 09:36:36 +0200

    Test deploy static pages

---
+++ precache-manifest.fc3bd841b0c966677a4dcba42e564b26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770cdcfde12b448e28dd",
+    "url": "/css/app.61d5fda1.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2635b128366e262356ecff68d5d1e0b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9a17e8cabae9695dd47",
+    "url": "/js/about.977162d2.js"
+  },
+  {
+    "revision": "770cdcfde12b448e28dd",
+    "url": "/js/app.1ef7cdfa.js"
+  },
+  {
+    "revision": "575f5adb4448e007eeed",
+    "url": "/js/chunk-vendors.9cb215fb.js"
+  },
+  {
+    "revision": "5a872ab2bd9e385f77ecb312e774783c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
